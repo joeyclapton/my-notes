9a553efe2a66b3d1b25ebd2264eb17743c701956
Joey Clapton <joeyclapton42@gmail.com>
2023-11-16 11:24:55 -0300

    feat: implements notes list

--- src/app/components/note/note.component.ts
+++ src/app/components/note/note.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +9,6 @@ import { CommonModule } from '@angular/common';
   styleUrl: './note.component.sass'
 })
 export class NoteComponent {
-
+  @Input() title: string = '';
+  @Input() text: string = '';
 }
--- src/app/components/notes-list/notes-list.component.ts
+++ src/app/components/notes-list/notes-list.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NoteComponent } from '../note/note.component';
+import { NoteService } from '../../services/note/note.service';
+import { Note } from '../../interface/note.interface';
 
 @Component({
   selector: 'app-notes-list',
@@ -9,6 +11,14 @@ import { NoteComponent } from '../note/note.component';
   templateUrl: './notes-list.component.html',
   styleUrl: './notes-list.component.sass'
 })
-export class NotesListComponent {
+export class NotesListComponent implements OnInit {
+ noteService = inject(NoteService)
+ notes: Note[] = []
 
+ constructor() {}
+ 
+ 
+ ngOnInit(): void {
+   this.notes = this.noteService.getAll();
+ }
 }
