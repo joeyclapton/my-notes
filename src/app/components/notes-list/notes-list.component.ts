efdbcd11020a2a0d0283a9490c8b8fedd36877ba
Joey Clapton <joeyclapton42@gmail.com>
2023-11-20 20:10:13 -0300

    feat: create submit validation

--- src/app/components/notes-list/notes-list.component.ts
+++ src/app/components/notes-list/notes-list.component.ts
@@ -3,32 +3,34 @@ import { CommonModule } from '@angular/common';
 import { NoteComponent } from '../note/note.component';
 import { NoteService } from '../../services/note/note.service';
 import { Note } from '../../interface/note.interface';
-import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-notes-list',
   standalone: true,
   imports: [CommonModule, NoteComponent, ReactiveFormsModule],
   templateUrl: './notes-list.component.html',
-  styleUrl: './notes-list.component.sass'
+  styleUrl: './notes-list.component.sass',
 })
 export class NotesListComponent implements OnInit {
-
-  noteService = inject(NoteService)
-  notes: Note[] = []
+  noteService = inject(NoteService);
+  notes: Note[] = [];
   formNotes = new FormGroup({
-    title: new FormControl(''),
-    description: new FormControl('')
-  })
+    title: new FormControl('', Validators.required),
+  });
 
-  constructor() { }
-
+  constructor() {}
 
   ngOnInit(): void {
     this.notes = this.noteService.getAll();
   }
 
   onSave() {
-    console.log(this.formNotes)
+    if (this.formNotes.valid) console.log(this.formNotes.value.title);
   }
 }
