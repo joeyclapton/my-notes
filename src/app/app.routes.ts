23aaad2d188450e780bdea17083a3f46f5af2cf2
Joey Clapton <joeyclapton42@gmail.com>
2023-11-21 17:00:04 -0300

    feat: add default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './pages/home/home/home.component';
 import { ContactComponent } from './pages/contact/contact/contact.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
 ];
