1486e6bdd83ff35a5c9e5b85e66a7d769c171f67
Joey Clapton <joeyclapton42@gmail.com>
2023-11-21 14:39:35 -0300

    feat: implements angular router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { NotesListComponent } from './components/notes-list/notes-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterOutlet, HeaderComponent, NotesListComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterOutlet,
+    HeaderComponent,
+    NotesListComponent,
+    RouterModule,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
 })
-
 export class AppComponent {
   title = 'my-notes';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home/home.component';
+import { ContactComponent } from './pages/contact/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+];
