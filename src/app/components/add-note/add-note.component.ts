9b5a7ebf9dc394ccac8fcab4fdc402c0090a1867
Joey Clapton <joeyclapton42@gmail.com>
2023-11-24 14:12:16 -0300

    feat: add redirect new note to home

--- src/app/components/add-note/add-note.component.ts
+++ src/app/components/add-note/add-note.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-note',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './add-note.component.html',
-  styleUrl: './add-note.component.sass'
+  styleUrl: './add-note.component.sass',
 })
 export class AddNoteComponent {
+  router = inject(Router);
 
+  addNote() {
+    this.router.navigateByUrl('');
+  }
 }
