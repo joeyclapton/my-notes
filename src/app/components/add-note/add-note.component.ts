cb61e19048fef4c0265793f02704ebef5120f023
Joey Clapton <joeyclapton42@gmail.com>
2023-11-24 14:33:02 -0300

    feat: create form for add note

--- src/app/components/add-note/add-note.component.ts
+++ src/app/components/add-note/add-note.component.ts
@@ -1,18 +1,37 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { NoteService } from '../../services/note/note.service';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  Validators,
+  FormGroup,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-note',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-note.component.html',
   styleUrl: './add-note.component.sass',
 })
 export class AddNoteComponent {
   router = inject(Router);
+  noteService = inject(NoteService);
+
+  form = new FormGroup({
+    title: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+  });
 
   addNote() {
-    this.router.navigateByUrl('');
+    if (this.form.valid) {
+      this.noteService.create({
+        title: this.form.value.title as string,
+        description: this.form.value.description as string,
+      });
+      this.router.navigateByUrl('');
+    }
   }
 }
